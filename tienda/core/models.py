fe2ae237cae0aee2097e4daefa4f9c006a9085e8
joss0103 <jo.rebolledoa@duocuc.cl>
2022-06-13 12:23:54 -0400

    commit -m "ARREGLANDO MODELOS"

--- tienda/core/models.py
+++ tienda/core/models.py
@@ -1,41 +1,52 @@
 
+from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import CharField
 from django.utils import timezone
 # Create your models here.
 class Categoria(models.Model):
-    id_Categoria = models.IntegerField(primary_key=True, verbose_name='ID CATEGORIA:')
-    categoria = models.CharField(default='NombreCategoria', max_length=25)
+    id_Categoria = models.AutoField(primary_key=True, verbose_name='ID CATEGORIA:')
+    categoria = models.CharField(default='NombreCategoria', max_length=25, verbose_name='CATEGORIA: ')
 
     def __str__(self):
         return self.categoria
 
+class SubCategoria(models.Model):
+    idSubcategoria = models.AutoField(primary_key=True, verbose_name='ID SUBCATEGORIA:')
+    subCategoria= models.CharField(max_length=50, verbose_name='SUBCATEGORIA: ')
 
+
+class cliente (models.Model):
+    rut = models.CharField(primary_key=True, verbose_name= 'RUT: ')
+    nombre=  models.CharField (max_length=50, verbose_name= 'NOMBRE: ')
+    telefono = models.CharField(max_length=12, verbose_name='TELEFONO: ')
+    correo= models.EmailField (max_length=80, verbose_name='CORREO ELECTRONICO: ' )
+    suscripcion= models.BooleanField
 
 class usuario(models.Model):
-    usuario =  models.CharField(primary_key=True, max_length=35, verbose_name='USUARIO: ')
+    usuario =  models.AutoField(primary_key=True, verbose_name='USUARIO: ')
     contrasenia= models.CharField(max_length=15, verbose_name='CONTRASENIA: ')
 
 class Producto(models.Model):
-    serie_producto = models.CharField(primary_key=True, max_length=10)
-    nombre_producto = models.CharField(max_length=30)
-    descripcion = models.TextField()
-    codigo = models.CharField(max_length=10)
-    modelo = models.CharField(max_length=20)
-    valor = models.IntegerField()
+    idproducto = models.AutoField(primary_key=True, verbose_name='ID PRODUCTO:')
+    nombre_producto = models.CharField(max_length=30, verbose_name= 'NOMBRE PRODUCTO: ')
+    descripcion = models.TextField(max_length=200, verbose_name='DESCRIPCION: ')
+    valor = models.IntegerField(max_length=10, verbose_name='VALOR: ')
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+    subcategoria=models.ForeignKey(SubCategoria, on_delete=models.PROTECT)
     imagen = models.ImageField(upload_to="productos", null=True)
-    valor_oferta = models.IntegerField(blank=True, null=True)
-    oferta = models.BooleanField()
-    nuevo = models.BooleanField()
+    valor_oferta = models.IntegerField(blank=True, null=True, verbose_name='VALOR OFERTA: ')
+    oferta = models.BooleanField(max_length= 10, verbose_name='OFERTA: ')
+    stock = models.IntegerField(max_length= 100, verbose_name= 'STOCK: ')
 
     def __str__(self):
         return self.nombre_producto
 
 
 class Pedido(models.Model):
-    num_pedido = models.IntegerField(primary_key=True)
-    despacho_domicilio = models.BooleanField()
+    idpedido = models.AutoField(primary_key=True)
+    despacho= models.BooleanField()
     forma_pago = models.BooleanField()
     pedido_aceptado = models.BooleanField()
     direccion_despacho = models.CharField(max_length=70)
@@ -47,6 +58,7 @@ class Pedido(models.Model):
         return self.num_pedido
 
 class Contacto(models.Model):
+    idcontacto = models.IntegerField(primary_key=True, max_length=20)
     nombre = models.CharField(max_length=25)
     email = models.CharField(max_length=35)
     consulta = models.TextField()
@@ -56,6 +68,7 @@ class Contacto(models.Model):
 
 
 class Venta(models.Model):
+    idventa= models.IntegerField(primary_key=True, max_length=25)
     valor = models.IntegerField()
     fecha = models.DateTimeField(default=timezone.now)
     pedido = models.ForeignKey(Pedido, on_delete=models.PROTECT, related_name='pedidoVenta')
@@ -63,3 +76,4 @@ class Venta(models.Model):
 
     def __str__(self):
         return self.cliente
+
