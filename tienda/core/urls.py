05a4030a58871bddf9104cfdf11006ee2df043e6
joss0103 <jo.rebolledoa@duocuc.cl>
2022-06-28 23:26:46 -0400

    commit -m "probando"

--- tienda/core/urls.py
+++ tienda/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, productos, crearp
+from .views import home, productos, crearp, edit_product
 
 urlpatterns = [
     path('', home,name="home"),
      path('productos/', productos,name="productos"),
      path('crear/', crearp, name="crearp" ),
+     path('actualizar/<id>/', edit_product, name="edit_product"), 
 ]
