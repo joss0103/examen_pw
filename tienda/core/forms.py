4cfa4dc3b739a63cacfbe447abd71b4e7ef56642
joss0103 <jo.rebolledoa@duocuc.cl>
2022-06-29 20:30:29 -0400

    form

--- tienda/core/forms.py
+++ tienda/core/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from .models import Producto
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class FormsProducto(forms.ModelForm):
     class Meta:
@@ -9,4 +10,7 @@ class FormsProducto(forms.ModelForm):
         fields = '__all__'
 
 class CustomUserCreationForm(UserCreationForm):
-    pass
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
